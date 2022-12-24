19b66eafb37b93372a95875ad2fe6b132b4cd9ae
R Subramanian <rvanush3@gmail.com>
2022-12-24 20:14:07 +0530

    Create For Loop.c

---
+++ For Loop.c
@@ -0,0 +1,17 @@
+ int i,j,k; //identation block
+    int a,b;
+    char *ch[]={"null","one","two","three","four","five",
+    "six","seven","eight","nine"};
+    scanf("%d%d",&a,&b);
+
+    for(i=a;i<=b;i++)
+    {
+        if(i>9 && i%2)
+            printf("odd\n");
+        else if(i>9 && i%2==0)
+            printf("even\n");
+        else
+            printf("%s\n",ch[i]);
+
+    }
+    return 0;
