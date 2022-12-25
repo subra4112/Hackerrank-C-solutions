5816e6aef82f3a46ff9e6e608db811b96577d331
R Subramanian <rvanush3@gmail.com>
2022-12-25 22:23:56 +0530

    Create Bitwise operators.c

---
+++ Bitwise operators.c
@@ -0,0 +1,18 @@
+int and=0,or=0,xor=0;
+  for(int i=1;i<n;i++)
+  {
+      
+      for(int j=i+1;j<n;j++)
+      {
+          if((i&j)> and && (i&j)<k){
+              and=i&j;
+          }
+          if((i|j)> or && (i|j)<k){
+              or=i|j;
+          }
+          if((i^j)> xor && (i^j)<k){
+              xor=i^j;
+             }       }
+  }
+   printf("%d\n%d\n%d",and,or,xor);
+}
